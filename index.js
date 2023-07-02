6e90ede5189da17c9f57cdddee33a832f83cf1de
Nick Adams <nick.pappas.adams@gmail.com>
2023-07-02 10:02:01 -0700

    finished functionality

--- index.js
+++ index.js
@@ -1,21 +1,41 @@
-import { Configuration, OpenAIApi } from 'openai'
-import dotenv from 'dotenv'
+import openai from './config/open-ai.js'
+import readlineSync from 'readline-sync'
+import colors from 'colors'
 
-dotenv.config()
-const configuration = new Configuration({
-    apiKey: process.env.OPENAI_API_KEY
-})
-
-const openai = new OpenAIApi(configuration)
 
 const main = async () => {
-    const chatCompletion = await openai.createChatCompletion({
-        model: 'gpt-3.5-turbo',
-        messages: [
-            { role: 'user', content: 'what is the capital of california?' }
-        ]
-    })
-    console.log(chatCompletion)
+    console.log(colors.bold.green('Welcome to the chat bot! Ask me anything.'))
+    console.log(colors.bold.green('You can now chat with me'))
+
+    const chatHistory = []
+
+    while(true) {
+        const messages = chatHistory.map(([role, content]) => ({ role, content }))
+        
+
+        const userInput = readlineSync.question(colors.yellow('You: '))
+        messages.push({ role: 'user', content: userInput })
+        
+        try {
+            const completion = await openai.createChatCompletion({
+                model: 'gpt-3.5-turbo',
+                messages: messages
+            })
+
+            const completionText = completion.data.choices[0].message.content
+            
+            if(userInput.toLowerCase() === 'quit') {
+                console.log(colors.green('Bot: ') + completionText)
+                return
+            }
+
+            console.log(colors.green('Bot: ') + completionText)
+            chatHistory.push(['user', userInput])
+            chatHistory.push(['assistant', completionText])
+        } catch(err) {
+            console.error(colors.red(err))
+        }
+    }
 }
 
 main()
